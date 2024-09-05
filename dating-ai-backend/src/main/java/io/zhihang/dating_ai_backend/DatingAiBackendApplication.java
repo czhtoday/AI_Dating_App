263ee38113bfd0db437e059ecd3f9df685b85225
Zhihang CHENG <zhihang@Zhihangs-MacBook-Pro.local>
2024-09-04 19:18:26 -0700

    initial the backend

---
+++ dating-ai-backend/src/main/java/io/zhihang/dating_ai_backend/DatingAiBackendApplication.java
@@ -0,0 +1,13 @@
+package io.zhihang.dating_ai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatingAiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatingAiBackendApplication.class, args);
+	}
+
+}
---
+++ dating-ai-backend/src/test/java/io/zhihang/dating_ai_backend/DatingAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.zhihang.dating_ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatingAiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
